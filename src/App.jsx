c1f113f3b1fa26d9eaba7a3445c34c41eacc439e
SantiagoVelasquezMontoya <santiago.velasquez@ias.com.co>
2023-03-19 16:00:23 -0500

    absolute rendering list items / position not right

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,11 @@
-import './App.css';
 import VirtualizedList from './components/VirtualizedList/VirtualizedList';
 
 function App() {
   const items = Array.from({ length: 1000 }).map((_, i) => `Person ${i}`);
   return (
     <div className='App'>
-      <h1>List Virtualization</h1>
-      <VirtualizedList items={items} itemHeight={35} />
+      <h1>Virtualized List</h1>
+      <VirtualizedList items={items} itemHeight={100} />
     </div>
   );
 }
--- src/components/VirtualizedList/VirtualizedList.jsx
+++ src/components/VirtualizedList/VirtualizedList.jsx
@@ -1,19 +1,19 @@
-import React, { useState, useRef } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import './VirtualizedList.sass';
 
 export default function VirtualizedList({ items, itemHeight }) {
-  const [visibleItems, setVisibleItems] = useState(items.slice(0, 6));
+  const [visibleItems, setVisibleItems] = useState(items.slice(0, 14));
   const outerContainerRef = useRef();
   const listContainerHeight = items.length * itemHeight;
 
   function handleScroll() {
-    console.log('Scrolling');
     const scrollTop = outerContainerRef.current.scrollTop;
     const scrollBottom = scrollTop + outerContainerRef.current.clientHeight;
 
     const startIndex = Math.floor(scrollTop / itemHeight);
     const endIndex = Math.ceil(scrollBottom / itemHeight);
     console.log(startIndex, endIndex);
+    setVisibleItems(items.slice(startIndex, endIndex));
   }
 
   return (
@@ -25,7 +25,11 @@ export default function VirtualizedList({ items, itemHeight }) {
       <div className='list-container' style={{ height: listContainerHeight }}>
         {visibleItems.map((item, index) => {
           return (
-            <div key={item} className='list-item'>
+            <div
+              key={item}
+              className='list-item'
+              style={{ height: itemHeight, top: index * itemHeight }}
+            >
               {item}
             </div>
           );
